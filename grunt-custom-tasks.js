e6b92961f9230d2579ca9dc2d808258a4735975d
Olivier van Helden (talents) <olivier@van-helden.net>
2025-06-30 20:33:57 +0000

    olib-dev to grunt-translations-tasks migration process (298)

--- grunt-custom-tasks.js
+++ grunt-custom-tasks.js
@@ -2,7 +2,7 @@
  * Backward compatibility alias for grunt-wp-plugin-tasks.js
  * 
  * This file is maintained for backward compatibility with existing WordPress plugins
- * that are already using grunt-custom-tasks.js.
+ * that are already using grunt-translation-tasks.js.
  * 
  * For new projects:
  * - Use grunt-wp-plugin-tasks.js for WordPress plugins  
